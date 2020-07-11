95df3a0e7954be0d9ae76e8dc716ebfa97d40afe
shubh10 <shubhagrawal101220001@gmail.com>
2020-07-12 01:39:30 +0530

    Updates

---
+++ precache-manifest.ab114fa16693a539933eead9b747afd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a6d49fd0157bb5c5dd76fcd5096748",
+    "url": "/othello/index.html"
+  },
+  {
+    "revision": "0c8689d074d2228a1a4d",
+    "url": "/othello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3929ab994b90d781eae",
+    "url": "/othello/static/js/2.5a33dd4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/othello/static/js/2.5a33dd4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8689d074d2228a1a4d",
+    "url": "/othello/static/js/main.df86562f.chunk.js"
+  },
+  {
+    "revision": "db941af7a2f850537a0d",
+    "url": "/othello/static/js/runtime-main.f55542f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/othello/static/media/logo.5d5d9eef.svg"
+  }
+]);
